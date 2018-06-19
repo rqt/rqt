81775d8bc736ce109331375751a0bf72efa7c2a0
Anton <anton@adc.sh>
2018-06-19 18:56:46 +0100

    update test

--- test/context/index.js
+++ test/context/index.js
@@ -1,18 +1,20 @@
-const http = require('http')
-const enableDestroy = require('server-destroy')
-const Catchment = require('catchment')
+import { createServer } from 'http'
+import enableDestroy from 'server-destroy'
+import Catchment from 'catchment'
 
 const createHandler = (getEndData = () => '200') => {
   const params = {
     postPromise: null,
     called: 0,
+    json: false,
+    headers: {},
   }
   return {
     handler: async (req, res) => {
       params.called += 1
-      params.json = req.headers['content-type'] === 'application/json'
+      params.json = req.headers['content-type'] == 'application/json'
       params.headers = req.headers
-      if (req.method === 'POST') {
+      if (req.method == 'POST') {
         const catchment = new Catchment
         req.pipe(catchment)
         const { promise } = catchment
@@ -34,26 +36,35 @@ const createHandler = (getEndData = () => '200') => {
   }
 }
 
-const context = async function Context() {
-  this.called = 0
-  this.data = '200'
-  const { handler, params } = createHandler(() => this.data)
-  this.params = params
-  const server = http.createServer(handler)
-  this.server = server
-  enableDestroy(this.server)
-
-  this._destroy = async () => {
+export default class Context {
+  constructor() {
+    this.called = 0
+    this.data = '200'
+  }
+  async _destroy() {
     await new Promise((resolve) => {
       this.server.destroy()
       this.server.on('close', resolve)
     })
   }
-  await new Promise((resolve) => {
-    server.listen(undefined, 'localhost', resolve)
-  })
-  this.address = server.address()
-  this.url = `http://${this.address.address}:${this.address.port}`
-}
+  getParams() {
+    return this.params
+  }
+  setData(data) {
+    this.data = data
+  }
+  async _init() {
+    const { handler, params } = createHandler(() => this.data)
+    this.params = params
 
-module.exports = context
+    const server = createServer(handler)
+    this.server = server
+    enableDestroy(this.server)
+
+    await new Promise((resolve) => {
+      this.server.listen(undefined, 'localhost', resolve)
+    })
+    this.address = server.address()
+    this.url = `http://${this.address.address}:${this.address.port}`
+  }
+}
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,21 +1,23 @@
-const { assert, equal, deepEqual, throws } = require('zoroaster/assert')
-const rqt = require('../../src/')
-const context = require('../context/')
-const { version } = require('../../package.json')
+import { ok, assert, equal, deepEqual, throws } from 'zoroaster/assert'
+import rqt from '../../src'
+import Context from '../context'
+import { version } from '../../package.json'
 
-const rqtTestSuite = {
-  context,
+/** @type {Object.<string, (c: Context)>} */
+const T = {
+  context: Context,
   async 'should be a function'() {
     equal(typeof rqt, 'function')
   },
-  async 'should request data from server' (ctx) {
-    const testData = 'test-data'
-    ctx.data = testData
-    const res = await rqt(ctx.url)
-    assert(ctx.params.called)
-    equal(res, testData)
+  async 'requests data from server' ({ setData, getParams, url }) {
+    const data = 'test-data'
+    setData(data)
+    const res = await rqt(url)
+    const { called } = getParams()
+    ok(called)
+    equal(res, data)
   },
-  async 'should fail'() {
+  async 'fails when ENOTFOUND'() {
     const url = `http://not-a-valid-web-page-${Math.floor(Math.random() * 10000)}.io`
     await throws({
       fn: rqt,
@@ -23,69 +25,78 @@ const rqtTestSuite = {
       code: 'ENOTFOUND',
     })
   },
-  async 'should request data from https'() {
+  async 'requests data from https'() {
     const url = 'https://google.com'
     const res = await rqt(url)
     assert(/The document has moved/.test(res))
   },
-  async 'should send post data'(ctx) {
+  async 'sends post data'({ getParams, url }) {
     const data = 'test post data'
-    const res = await rqt(ctx.url, {
+    const res = await rqt(url, {
       data,
       contentType: 'application/x-www-form-urlencoded',
     })
-    assert(ctx.params.called)
+    const { called } = getParams()
+    ok(called)
     equal(res, data)
   },
-  async 'should parse json data'(ctx) {
-    const rawData = { data: 'test post data' }
-    const data = JSON.stringify(rawData)
-    const res = await rqt(ctx.url, {
+  async 'parses json data'({ url, getParams }) {
+    const d = { data: 'test post data' }
+    const data = JSON.stringify(d)
+    const res = await rqt(url, {
       data,
     })
-    assert(ctx.params.called)
-    deepEqual(res, rawData)
+    const { called } = getParams()
+    ok(called)
+    deepEqual(res, d)
   },
-  async 'should reject when cannot parse json data'(ctx) {
+  async 'rejects when cannot parse json data'({ url }) {
     const data = 'not-json-data'
+    // await throws({
+    //   fn: rqt,
+    //   args: [url, { data }],
+    // })
     try {
-      await rqt(ctx.url, {
+      await rqt(url, {
         data,
       })
       throw new Error('Should have thrown an error')
     } catch ({ postData, message, stack }) {
       assert.equal(postData, data)
       assert(/Unexpected token o/.test(message))
-      assert(/ at should reject when cannot parse json data/.test(stack))
+      assert(/ at rejects when cannot parse json data/.test(stack))
     }
   },
-  async 'should send headers'(ctx) {
+  async 'sends headers'({ url, getParams }) {
     const testHeader = 'test post header'
-    await rqt(ctx.url, {
+    await rqt(url, {
       data: 'test',
       contentType: 'application/x-www-form-urlencoded',
       headers: {
         'x-test': testHeader,
       },
     })
-    equal(ctx.params.headers['x-test'], testHeader)
+    const { headers } = getParams()
+    equal(headers['x-test'], testHeader)
   },
-  async 'should send user-agent'(ctx) {
+  async 'sends user-agent'({ url, getParams }) {
     const expected = `Mozilla/5.0 (Node.js) rqt/${version}`
-    await rqt(ctx.url, {
+    await rqt(url, {
       data: 'test',
       contentType: 'application/x-www-form-urlencoded',
     })
-    equal(ctx.params.headers['user-agent'], expected)
+    const { headers } = getParams()
+    equal(headers['user-agent'], expected)
   },
-  async 'should request github data'() {
+  async 'requests github data'() {
     const res = await rqt('https://api.github.com/users/octocat/orgs')
     equal(res, '[]')
   },
-  async 'should return binary data'(ctx) {
-    ctx.data = 'test buffer'
-    const expected = new Buffer(ctx.data)
-    const res = await rqt(ctx.url, {
+  async 'returns binary data'({ url, setData }) {
+    const d = 'test buffer'
+    setData(d)
+    const expected = new Buffer(d)
+    const res = await rqt(url, {
       binary: true,
     })
     assert(res instanceof Buffer)
@@ -93,4 +104,4 @@ const rqtTestSuite = {
   },
 }
 
-module.exports = rqtTestSuite
+export default T
