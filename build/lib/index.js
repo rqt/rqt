453330dc26882b35bcb67940faa0fbc353033030
Anton <anton@adc.sh>
2019-07-29 21:07:07 +0300

    b

--- build/lib/index.js
+++ build/lib/index.js
@@ -1,4 +1,4 @@
-let aqt = require('@rqt/aqt'); if (aqt && aqt.__esModule) aqt = aqt.default;
+const aqt = require('@rqt/aqt');
 
 /**
  * Request an HTTP page and return the response body as a string.
@@ -6,12 +6,12 @@ let aqt = require('@rqt/aqt'); if (aqt && aqt.__esModule) aqt = aqt.default;
  * @param {Options} [options] Options for requests.
  * @param {*} [options.data] Optional data to send to the server with the request.
  * @param {'form'|'json'} [options.type="'json'"] How to send data: `json` to serialise JSON data and `form` for url-encoded transmission with `json` mode by default. Default `'json'`.
- * @param {OutgoingHttpHeaders} [options.headers] Headers to use for the request.
+ * @param {http.OutgoingHttpHeaders} [options.headers] Headers to use for the request.
  * @param {boolean} [options.compress=true] Add the `Accept-Encoding: gzip, deflate` header to indicate to the server that it can send a compressed response. Default `true`.
  * @param {string} [options.method] What HTTP method to use in making of the request. When no method is given and `data` is present, defaults to `POST`.
  * @param {number} [options.timeout] Timeout after which the request should cancel.
  */
-       const rqt = async (address, options = {}) => {
+const rqt = async (address, options = {}) => {
   const { body } = await aqt(address, options)
   /** @type {string} */
   const r = body
@@ -24,12 +24,12 @@ let aqt = require('@rqt/aqt'); if (aqt && aqt.__esModule) aqt = aqt.default;
  * @param {Options} [options] Options for requests.
  * @param {*} [options.data] Optional data to send to the server with the request.
  * @param {'form'|'json'} [options.type="'json'"] How to send data: `json` to serialise JSON data and `form` for url-encoded transmission with `json` mode by default. Default `'json'`.
- * @param {OutgoingHttpHeaders} [options.headers] Headers to use for the request.
+ * @param {http.OutgoingHttpHeaders} [options.headers] Headers to use for the request.
  * @param {boolean} [options.compress=true] Add the `Accept-Encoding: gzip, deflate` header to indicate to the server that it can send a compressed response. Default `true`.
  * @param {string} [options.method] What HTTP method to use in making of the request. When no method is given and `data` is present, defaults to `POST`.
  * @param {number} [options.timeout] Timeout after which the request should cancel.
  */
-       const jqt = async (address, options = {}) => {
+const jqt = async (address, options = {}) => {
   const { body } = await aqt(address, options)
   /** @type {*} */
   const r = body
@@ -42,12 +42,12 @@ let aqt = require('@rqt/aqt'); if (aqt && aqt.__esModule) aqt = aqt.default;
  * @param {Options} [options] Options for requests.
  * @param {*} [options.data] Optional data to send to the server with the request.
  * @param {'form'|'json'} [options.type="'json'"] How to send data: `json` to serialise JSON data and `form` for url-encoded transmission with `json` mode by default. Default `'json'`.
- * @param {OutgoingHttpHeaders} [options.headers] Headers to use for the request.
+ * @param {http.OutgoingHttpHeaders} [options.headers] Headers to use for the request.
  * @param {boolean} [options.compress=true] Add the `Accept-Encoding: gzip, deflate` header to indicate to the server that it can send a compressed response. Default `true`.
  * @param {string} [options.method] What HTTP method to use in making of the request. When no method is given and `data` is present, defaults to `POST`.
  * @param {number} [options.timeout] Timeout after which the request should cancel.
  */
-       const bqt = async (address, options) => {
+const bqt = async (address, options) => {
   const c = {
     ...options,
     binary: true,
@@ -64,12 +64,12 @@ let aqt = require('@rqt/aqt'); if (aqt && aqt.__esModule) aqt = aqt.default;
  * @param {Options} [config] Options for requests.
  * @param {*} [config.data] Optional data to send to the server with the request.
  * @param {'form'|'json'} [config.type="'json'"] How to send data: `json` to serialise JSON data and `form` for url-encoded transmission with `json` mode by default. Default `'json'`.
- * @param {OutgoingHttpHeaders} [config.headers] Headers to use for the request.
+ * @param {http.OutgoingHttpHeaders} [config.headers] Headers to use for the request.
  * @param {boolean} [config.compress=true] Add the `Accept-Encoding: gzip, deflate` header to indicate to the server that it can send a compressed response. Default `true`.
  * @param {string} [config.method] What HTTP method to use in making of the request. When no method is given and `data` is present, defaults to `POST`.
  * @param {number} [config.timeout] Timeout after which the request should cancel.
  */
-       const srqt = async (address, config) => {
+const srqt = async (address, config) => {
   throw new Error('not implemented')
   // config.binary = true
   // const  { body } = await arqt(address, config)
@@ -78,15 +78,18 @@ let aqt = require('@rqt/aqt'); if (aqt && aqt.__esModule) aqt = aqt.default;
 
 /* documentary types/options.xml */
 /**
- * @typedef {import('http').OutgoingHttpHeaders} OutgoingHttpHeaders
- *
+ * @suppress {nonStandardJsDocs}
  * @typedef {Object} Options Options for requests.
  * @prop {*} [data] Optional data to send to the server with the request.
  * @prop {'form'|'json'} [type="'json'"] How to send data: `json` to serialise JSON data and `form` for url-encoded transmission with `json` mode by default. Default `'json'`.
- * @prop {OutgoingHttpHeaders} [headers] Headers to use for the request.
+ * @prop {http.OutgoingHttpHeaders} [headers] Headers to use for the request.
  * @prop {boolean} [compress=true] Add the `Accept-Encoding: gzip, deflate` header to indicate to the server that it can send a compressed response. Default `true`.
  * @prop {string} [method] What HTTP method to use in making of the request. When no method is given and `data` is present, defaults to `POST`.
  * @prop {number} [timeout] Timeout after which the request should cancel.
+ */
+/**
+ * @suppress {nonStandardJsDocs}
+ * @typedef {import('http').OutgoingHttpHeaders} http.OutgoingHttpHeaders
  */
 
 
